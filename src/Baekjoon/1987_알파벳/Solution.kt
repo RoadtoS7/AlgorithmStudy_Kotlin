131010cc3a560224c06c4f9d47357c05dafb6ad5
RoadtoS7 <kshvv975@gmail.com>
2021-09-14 10:24:42 +0900

    Add Baekjoon/1987_알파벳

--- src/Baekjoon/1987_알파벳/Solution.kt
+++ src/Baekjoon/1987_알파벳/Solution.kt
@@ -1,32 +1,50 @@
 package Baekjoon.`1987_알파벳`
 
-import java.lang.Integer.max
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import kotlin.math.max
+import java.util.*
 
 class Solution {
+    data class Point(
+        val row: Int,
+        val column: Int,
+        val path: String
+    )
 
-    fun solution() {
-        val inputs = readLine()!!.split(" ").map { it.toInt() }
-        val R = inputs[0]
-        val C = inputs[1]
-        val direction = listOf(0 to 1, 0 to -1, 1 to 0, -1 to 0)
-        var maxPath = -1
+    private val dirR = listOf(0, 0, 1, -1)
+    private val dirC = listOf(1, -1, 0, 0)
+    private var maxPath = -1
 
-        val map = IntRange(1, R).map { readLine()!!.toList() }
+    fun solution() = with(BufferedReader(InputStreamReader(System.`in`))) {
+        val st = StringTokenizer(readLine())
+        val R = st.nextToken().toInt()
+        val C = st.nextToken().toInt()
+        val map = Array(R) { Array(C) { " " } }
+        val cache = Array(R) { Array(C) { " " } }
 
-        fun dfs(map: List<List<Char>>, r: Int, c: Int, path: Set<Char>) {
-            maxPath = max(maxPath, path.size)
+        for (i in 0 until R) {
+            map[i] = readLine().toCharArray().map{ it.toString() }.toTypedArray()
+        }
 
-            for (dir in direction){
-                val newR = r + dir.first
-                val newC = c + dir.second
+        val needvisit = LinkedList<Point>()
+        needvisit.add(Point(0, 0, map[0][0]))
 
-                if (newR >= 0 && newR < R && newC >= 0 && newC < C && map[newR][newC] !in  path) {
-                    dfs(map, newR, newC, path.toMutableSet().apply { add(map[newR][newC]) })
+        while (needvisit.isNotEmpty()) {
+            val pair = needvisit.poll()
+            maxPath = max(maxPath, pair.path.length)
+
+            for (i in 0 until 4) {
+                val newR = pair.row + dirR[i]
+                val newC = pair.column + dirC[i]
+
+                if (newR >= 0 && newR < R && newC >= 0 && newC < C && !pair.path.contains(map[newR][newC])) {
+                    cache[newR][newC] = (pair.path + map[newR][newC])
+                    needvisit.addLast(Point(newR, newC, pair.path + map[newR][newC]))
                 }
             }
         }
 
-        dfs(map, 0, 0, mutableSetOf(map[0][0]))
         print(maxPath)
     }
 }
